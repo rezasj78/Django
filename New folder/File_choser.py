a793463680b1914beb4e31d0cab394b3cb062fdb
rezasj78 <sahraeianreza@gmail.com>
2020-01-07 19:19:57 +0330

    Remove duplicated directory

--- New folder/File_choser.py
+++
@@ -1,6 +0,0 @@
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-
-Tk().withdraw()
-file = open(askopenfilename(), 'r')
-print(file.read())
--- New folder/Json_help.py
+++
@@ -1,9 +0,0 @@
-import json
-
-JF = open("package.json", "r")
-j = json.load(JF)
-j = json.dumps(j["friends_phone"])
-JF.close()
-file = open("package2.json", "w")
-file.write(j)
-file.close()
